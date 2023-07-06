2c3be7aaa2fd304dd8476e81620ccf8c4b331d45
juliaszbrt <112975933+juliaszbrt@users.noreply.github.com>
2023-07-06 13:16:53 -0400

    CSS gradient

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 document.querySelector('form').addEventListener('submit', HandleSubmitForm);
+document.querySelector('ul').addEventListener('click', HandleClickDeleteOrCheck)
 
 function HandleSubmitForm(event) {
     event.preventDefault();
@@ -7,12 +8,37 @@ function HandleSubmitForm(event) {
     if (input.value != '') 
         AddToDo(input.value);
 
-    input.value = ';' // clears added text
+    input.value = '' // clears added text
 }
+
+function HandleClickDeleteOrCheck(event) {
+    if (event.target.name == "check") 
+        CheckToDo(event);
+
+    if (event.target.name == "delete")
+        DeleteToDo(event);
+}
+
+
 
 function AddToDo(todo) {
     let ul = document.querySelector('ul');
     let li = document.createElement('li');
 
-    
+    li.innerHTML = `
+        <span>${todo}</span>
+        <button name="check">done</button>
+        <button name="delete">delete</button>
+    `
+
+    ul.appendChild(li);
 }
+
+// function CheckToDo(e) {
+//     let item = e.target.parentNode;
+//     if ()
+// }
+
+// function DeleteToDo(del) {
+
+// }
