8b49704696045de930385e6029b61121ad1720b8
juliaszbrt <112975933+juliaszbrt@users.noreply.github.com>
2023-07-09 16:40:26 -0400

    trash/check not working

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+// Event Listeners
 document.querySelector('form').addEventListener('submit', HandleSubmitForm);
-document.querySelector('ul').addEventListener('click', HandleClickDeleteOrCheck);
+// document.querySelector('ul').addEventListener('click', HandleClickDeleteOrCheck);
 document.querySelector('a').addEventListener('click', ClearAll);
+document.getElementById('check').addEventListener('click', HandleCheck);
+document.getElementById('trash').addEventListener('click', HandleDelete);
 
+// Event Handlers
 function HandleSubmitForm(e) {
     e.preventDefault();
 
@@ -12,45 +16,61 @@ function HandleSubmitForm(e) {
     input.value = '' // clears added text
 }
 
-function HandleClickDeleteOrCheck(e) {
-    if (e.target.name == "check") 
-        CheckToDo(e);
+// function HandleCheck {
+    
+// }
 
-    if (e.target.name == "delete")
-        DeleteToDo(e);
-
+function HandleDelete() {
+    let trash = document.getElementById('trash');
+    trash.parentNode.remove();
 }
 
+
+
+
+
+
+// function HandleClickDeleteOrCheck(e) {
+//     if (e.target.name == "check") 
+//         CheckToDo(e);
+
+//     if (e.target.name == "delete")
+//         DeleteToDo(e);
+
+// }
+
+
+// Helpers
 function AddToDo(todo) {
     let ul = document.querySelector('ul');
     let li = document.createElement('li');
 
     li.innerHTML = `
         <span class="to-do-list">${todo}</span>
-        <button name="check"><img src=/images/check.png class="check"></button>
-        <button name="delete"><img src=/images/trash.png class="delete"></button>
+        <button name="check" id="check"><img src=/images/check.png class="check"></button>
+        <button name="delete" id="delete"><img src=/images/trash.png class="delete"></button>
     `
 
     ul.appendChild(li);
 }
 
-function CheckToDo(e) {
-    let item = e.target.parentNode;
-    if (item.style.textDecoration == 'line-through') {
-        item.style.textDecoration = none;
-    }
+// function CheckToDo(e) {
+//     let item = e.target.parentNode;
+//     if (item.style.textDecoration == 'line-through') {
+//         item.style.textDecoration = none;
+//     }
 
-    else {
-        item.style.textDecoration = 'line-through';
-    }
-}
+//     else {
+//         item.style.textDecoration = 'line-through';
+//     }
+// }
 
-function DeleteToDo(e) {
-    let item = e.target.parentNode;
-    item.remove();
+// function DeleteToDo(e) {
+//     let item = e.target.parentNode;
+//     item.remove();
 
-}
+// }
 
-function ClearAll(e) {
+function ClearAll() {
     document.querySelector('ul').innerHTML = '';
 }
