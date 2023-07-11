056e72751c38267c7b212bf06fa08f8bbfe27c2f
juliaszbrt <112975933+juliaszbrt@users.noreply.github.com>
2023-07-11 11:26:57 -0400

    gave up on unique colors

--- app.js
+++ app.js
@@ -77,10 +77,9 @@ function handleOption1() {
         btn3.style.borderWidth = '1.5px';
 
         body.style.background = 'linear-gradient(323deg, rgba(255,207,126,1) 0%, rgba(255,139,162,1) 100%)';
-        document.getElementById('title').style.color = '#591212';
-        document.getElementById('clear').style.color = '#591212';
-        document.getElementById('input').style.placeholder = '';
-        document.querySelector('ul').style.background = '#FFD4D4'
+        // document.getElementById('title').style.color = '#591212';
+        // document.getElementById('clear').style.color = '#591212';
+        // document.querySelector('ul').style.background = '#FFD4D4'
         document.querySelector('li').style.color = '#591212';
     }
 }
@@ -96,10 +95,10 @@ function handleOption2() {
         btn3.style.borderWidth = '1.5px';
 
         body.style.background = 'linear-gradient(323deg, rgba(133,187,212,1) 0%, rgba(103,70,209,1) 100%)';
-        document.getElementById('title').style.color = '#1E0D4F';
-        document.getElementById('clear').style.color = '#1E0D4F';
+        // document.getElementById('title').style.color = '#1E0D4F';
+        // document.getElementById('clear').style.color = '#1E0D4F';
         document.getElementById('input').style.color = '#1E0D4F';
-        document.querySelector('ul').style.background = '#C2B3EA';
+        // document.querySelector('ul').style.background = '#C2B3EA';
         document.querySelector('li').style.color = '#1E0D4F';
 
         changePlaceholderColor();
@@ -117,14 +116,10 @@ function handleOption3() {
         btn2.style.borderWidth = '1.5px';
 
         body.style.background = 'linear-gradient(323deg, rgba(255,195,79,1) 0%, rgba(146,198,112,1) 83%, rgba(124,199,119,1) 100%)';
-        document.getElementById('title').style.color = '#274F0D';
-        document.getElementById('clear').style.color = '#274F0D';
+        // document.getElementById('title').style.color = '#274F0D';
+        // document.getElementById('clear').style.color = '#274F0D';
         document.getElementById('input').style.color = '#274F0D';
-        document.querySelector('ul').style.background = '#F4E289';
+        // document.querySelector('ul').style.background = '#F4E289';
         document.querySelector('li').style.color = '#274F0D';
     }
 }
-
-// function changePlaceholderColor() {
-//     document.getElementsByName('input')[0].placeholder = 'hello';
-// }
